471bde5075cd8a5236fcfb38c6fe831ec99b4b66
Nightcycle <77173389+nightcycle@users.noreply.github.com>
2022-05-03 11:30:04 -0400

    Built and published by Moonwave

---
+++ assets/js/5c1b838a.0fc243ce.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[238],{97868:function(s){s.exports=JSON.parse('{"sourceUrl":"https://github.com/nightcycle/midas/blob/master","baseUrl":"/midas/","classOrder":[],"apiCategories":[]}')}}]);
---
+++ assets/js/ab22e4ce.56e543e8.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[372],{82824:function(e){e.exports=JSON.parse('[{"type":"link","href":"/api/Midas","label":"\u200bMidas"},{"type":"link","href":"/api/Service","label":"\u200bService"}]')}}]);
