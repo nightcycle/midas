c93da8ff1e4cca4c07b72f6b7542b011bbb18bde
Nightcycle <77173389+nightcycle@users.noreply.github.com>
2022-09-07 02:39:18 -0600

    improved config api

--- src/Config.lua
+++ src/Config.lua
@@ -1,4 +1,11 @@
-return {
+--!strict
+local _Package = script.Parent
+local _Packages = _Package.Parent
+local Types = require(_Package.Types)
+
+local Config: Types.ConfigData = {
 	Version = 13, --Internal versioning for midas framework
 	SendDeltaState = true, --Send just the changes of state, not the entirety
 }
+
+return Config
