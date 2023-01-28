833d3cb7a5ae441422d5eb640ad08a69d04fa745
Nightcycle <77173389+nightcycle@users.noreply.github.com>
2023-01-27 17:08:55 -0700

    Added game stat tracking to recorder

--- scripts/recorder.py
+++ scripts/recorder.py
@@ -22,19 +22,24 @@ session.cookies.update({
 	".ROBLOSECURITY": RBX_AUTH_COOKIE
 })
 
+global universeId
+placeResponse = session.get(" https://games.roblox.com/v1/games/multiget-place-details?placeIds="+str(PLACE_ID))
+for place in json.loads(placeResponse.text):
+	universeId = place["universeId"]
+
+
 def dumpElement(element) -> str:
 	return (html.tostring(element)).decode('utf-8')
 
-def recordAds():
 
-	def getAds():
+def getAds():
 
-		response = session.get("https://www.roblox.com/develop/groups/"+str(GROUP_ID)+"?Page=ads")
+	response = session.get("https://www.roblox.com/develop/groups/"+str(GROUP_ID)+"?Page=ads")
+	tree = html.fromstring(response.text)
 
-		tree = html.fromstring(response.text)
+	ads = []
 
-		ads = []
-
+	try:
 		for tabl in tree.xpath('//table[@data-ad-type]'):
 			# get title
 			ad_id = tabl.get("data-item-id")
@@ -113,27 +118,81 @@ def recordAds():
 			}
 			if PLACE_ID == place_id:
 				ads.append(ad_data)
+	except:
+		print("Recording ads failed")
 
-		return ads
+	return ads
 
-	adData = {
-		"Timestamp": datetime.now().timestamp(),
-		"PlaceId": PLACE_ID,
-		"Advertisements":  getAds(),
-	}
 
-	file = open("./dashboard/input/marketing/advertisement/"+str(adData["Timestamp"]).replace(".", "_")+".json", "w")
-	file.write(json.dumps(adData, indent=4))
-	file.close()
+def getConcurrents(placeId: int) -> int: 
+	response = session.get(GAME_URL_PREFIX+str(placeId))
+	tree = html.fromstring(response.text)
+	
+	try:
+		for element in tree.xpath("//li[@class='game-stat game-stat-width-voice']"):
+			for stat in element.xpath(".//p[@class='text-label text-overflow font-caption-header']"):
+				if stat.text == "Active":
+					for val in element.xpath(".//p[@class='text-lead font-caption-body wait-for-i18n-format-render invisible']"):
+						return int(val.text.replace(",", ""))
+	except:
+		return 0
+
+	return 0
+
+def getGameStats():
+	global likes
+	global dislikes
+	global favorites
+	global visits
+	global concurrents
+
+	try:
+		# get like / dislike
+		for entry in json.loads(session.get("https://games.roblox.com/v1/games/votes?universeIds="+str(universeId)).text)["data"]:
+			if entry["id"] == universeId:
+				likes = entry["upVotes"]
+				dislikes = entry["downVotes"]
+
+		# get favorites
+		favorites = json.loads(session.get("https://games.roblox.com/v1/games/"+str(universeId)+"/favorites/count").text)["favoritesCount"]
+		
+		# parse for concurrents and visits
+		response = session.get(GAME_URL_PREFIX+str(PLACE_ID))
+		tree = html.fromstring(response.text)
+
+		# get visits
+		for element in tree.xpath("//p[@id='game-visit-count']"):
+			visits = int(element.get("title").replace(",", ""))
+
+		concurrents = getConcurrents(PLACE_ID)
+
+		return {
+			"Rating": {
+				"Likes": likes,
+				"Dislikes": dislikes,
+			},
+			"Favorites": favorites,
+			"Visits": visits,
+			"Concurrents": concurrents,
+		}
+	except:
+		return {}
 
 index = 0
 while True:
 	index += 1
 	print("Recording #"+str(index))
-	
-	try:
-		recordAds()
-	except:
-		print("Recording failed")
+		
+	data = {
+		"Timestamp": datetime.now().timestamp(),
+		"PlaceId": PLACE_ID,
+		"Advertisements": getAds(),
+		"Game": getGameStats(),
+	}
+
+	file = open("./dashboard/input/records/"+str(data["Timestamp"]).replace(".", "_")+".json", "w")
+	file.write(json.dumps(data, indent=4))
+	file.close()
+
 
 	time.sleep(RECORDING_INTERVAL)
