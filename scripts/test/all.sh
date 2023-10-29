640a738bd3b382713ba888ba4bb58f83ec09c9ab
Nightcycle <77173389+nightcycle@users.noreply.github.com>
2023-10-29 17:17:40 -0400

    Established vanilla table-based functionality with MongoDB

--- scripts/test/all.sh
+++ scripts/test/all.sh
@@ -2,6 +2,6 @@
 # sh scripts/test/download.sh
 # sh scripts/test/unit-test.sh
 sh scripts/test/lsp.sh src out
-sh scripts/test/lsp.sh out src
+# sh scripts/test/lsp.sh out src
 sh scripts/test/selene.sh src
-sh scripts/test/selene.sh out
+# sh scripts/test/selene.sh out
---
+++ scripts/workspace/rojo/build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+rojo build dev.project.json --output midas.rbxl
